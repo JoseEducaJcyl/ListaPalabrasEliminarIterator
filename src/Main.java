c20932658112564ce4fb82079bfb0c2da034a1f3
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-01-23 10:57:29 +0100

    Ejercicio sobre una lista de palabras con Iterator y eliminando por una vocal

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+        List<String> palabras = new ArrayList<String>();
+        palabras.add("Arbol");
+        palabras.add("Carrera");
+        palabras.add("Macarrones");
+        palabras.add("Telefono");
+        palabras.add("CPU");
+        Iterator<String> iterador = palabras.iterator();
+        while (iterador.hasNext()) {
+            String palabra = iterador.next();
+            for (int i = 0; i <= palabra.length()-1; i++) {
+                char letra = palabra.charAt(i);
+                if (letra == 'a' || letra == 'A') {
+                    iterador.remove();
+                    break;
+                }
+            }
+        }
+        for (String palabra : palabras) {
+            System.out.println(palabra);
+        }
+    }
 }
